17d0043a54abef7ac1b27eeb62c26277fcdaea1f
Noe Trevino <noetrevino.music@gmail.com>
2024-08-26 15:04:16 -0500

    progress on doubly linked list
    - still need to take care of edge cases
    - getting loops

--- linked-lists/exercise-2.py
+++ linked-lists/exercise-2.py
@@ -10,8 +10,11 @@ class DoublyLinkedList:
         self.head = head
 
     def push(self, data):
-        new_node = Node(data, None, self.head)
-        self.head = new_node
+        if self.head is None:
+            self.head = Node(data)
+
+        self.head.data = data
+        self.head.next = self.head
 
     def append(self, data):
         if self.head is None:
@@ -28,7 +31,24 @@ class DoublyLinkedList:
         for data in data_list:
             self.append(data)
 
-    def print(self):
+    def insert_after_value(self, data_after, data_to_insert):
+        # TODO: raise exception here if value does not exist, else where also
+        if self.head is None:
+            self.head = Node(data_to_insert)
+
+        itr = self.head
+        while itr.next:
+            # this is broken
+            if itr.data == data_after:
+                # itr.next = Node(data_to_insert, itr, itr.next)
+                itr.next.data = data_to_insert
+                itr.next.prev = itr
+                itr.next.next = itr.next
+                return
+
+            itr = itr.next
+
+    def print_forwards(self):
         llstr = ""
         itr = self.head
         if itr is None:
@@ -45,14 +65,35 @@ class DoublyLinkedList:
 
         print(llstr)
 
-    def insert_after_value(self, data):
-        if self.head is None:
-            self.head = Node(data)
+    def print_backwards(self):
+        itr = self.head
+        llstr = ""
+
+        if itr is None:
+            print("Linked List is empty")
+            return
+
+        # just traverse
+        while itr.next:
+            itr = itr.next
+
+        # traverse backwards
+        while itr:
+            llstr += str(itr.data)
+
+            if itr.prev is not None:
+                llstr += "<--"
+
+            itr = itr.prev
+
+        print(llstr)
 
 
 ll = DoublyLinkedList()
 ll.append(1)
 ll.append(2)
-ll.push(0)
-ll.append_values([0, 2, 1, 4, 32, 9])
-ll.print()
+# ll.push(0)
+ll.append_values([2, 1, 4, 32, 9])
+# ll.insert_after_value(9, "HERE")
+ll.print_forwards()
+ll.print_backwards()
